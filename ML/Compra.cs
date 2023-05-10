c66776eb5eec77d5b467daaea2fa09ddd41f717e
alejandragutierrezglez <alejandragutierrezglez2023@gmail.com>
2023-05-10 12:50:56 -0600

    Terminado

---
+++ ML/Compra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Compra
+    {
+        public string Tarjeta { get; set; }
+        public string CVV { get; set; }
+        public string FechaExpiracion { get; set; }
+        public string Banco { get; set; }
+        public string Calle { get; set; }
+        public string NumeroExterior { get; set; }
+        public string NumeroInterior { get; set; }
+        public string Colonia { get; set; }
+        public string Municipio { get; set; }
+        public string Estado { get; set; }
+        public string CodigoPostal { get; set; }
+    }
+}
---
+++ PL/Scripts/RealizarCompra.js
@@ -0,0 +1,28 @@
+﻿function Modal() {
+    $('#myModal').modal('show');
+    RealizarCompra();
+}
+
+function ModalClose() {
+    $('#myModal').modal('hide');
+}
+
+
+function RealizarCompra() {
+    var compra = {
+        Tarjeta: $('#txtTarjeta').val(''),
+        CVV: $('#txtCVV').val(''),
+        FechaExpiracion: $('#txtFechaExpiracion').val(''),
+        Banco: $('#txtBanco').val(''),
+        Calle: $('#txtCalle').val(''),
+        NumeroExterior: $('#txtNumeroExterior').val(''),
+        NumeroInterior: $('#txtNumeroInterior').val(''),
+        Colonia: $('#txtColonia').val(''),
+        Municipio: $('#txtMunicipio').val(''),
+        Estado: $('#txtEstado').val(''),
+        CodigoPostal: $('#txtCodigoPostal').val(''),
+    }
+}
+function Validar() {
+    $('#ModalOk').modal('show');
+}
